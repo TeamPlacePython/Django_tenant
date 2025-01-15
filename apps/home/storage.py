220d55467e516ccdac495d5e8c856c2ba16424f4
Laurent <jouronlaurent@hotmail.com>
2025-01-15 08:30:23 +0100

    first commit

--- apps/home/storage.py
+++ apps/home/storage.py
@@ -1,15 +1,24 @@
 from django.core.files.storage import FileSystemStorage
 from django.db import connection
 from django_tenants.files.storage import TenantFileSystemStorage
+from django.conf import settings
+from storages.backends.s3 import S3Storage
 
 
 class CustomSchemaStorage:
     def _get_storage_backend(self):
         schema_name = connection.schema_name
-        if schema_name == "public":
-            return FileSystemStorage()
-        else:
-            return TenantFileSystemStorage(schema_name)
+        if settings.ENVIRONMENT == "development":
+            return (
+                FileSystemStorage()
+                if schema_name == "public"
+                else TenantFileSystemStorage()
+            )
+        return (
+            S3Storage()
+            if schema_name == "public"
+            else S3TenantStorage(schema_name)
+        )
 
     def save(self, name, content, max_length=None):
         storage_backend = self._get_storage_backend()
@@ -26,3 +35,13 @@ class CustomSchemaStorage:
     def delete(self, name):
         storage_backend = self._get_storage_backend()
         storage_backend.delete(name)
+
+
+class S3TenantStorage(S3Storage):
+    def __init__(self, schema_name):
+        super().__init__()
+        self.schema_name = schema_name
+
+    def save(self, name, content, max_length=None):
+        path = f"{settings.MULTITENANT_RELATIVE_MEDIA_ROOT % self.schema_name}/{name}"
+        return super().save(path, content, max_length)
--- apps/tenant_manager/models.py
+++ apps/tenant_manager/models.py
@@ -6,6 +6,8 @@ from django_tenants.models import DomainMixin
 class Tenant(TenantMixin):
     name = models.CharField(max_length=100)
     created = models.DateField(auto_now_add=True)
+    auto_create_schema = True
+    auto_drop_schema = True
 
 
 class Domain(DomainMixin):
